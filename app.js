ea0224fe0065e71b980d5b673e8e52aabb278a59
Olga Murashova <om.dreyfus@gmail.com>
2022-07-16 23:06:17 -0400

    adding event listener

--- app.js
+++ app.js
@@ -13,6 +13,29 @@ function PageTransitions(){
             this.className += ' active-btn';
          })
     }
+    //sections Active class
+    for (const Section of allSections){
+    Section.addEventListener('click', function(event){
+        const id = event.target.dataset.id;
+        if(id){
+            //remove selected from the other buttons
+            secBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            event.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+            
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+}
+
+    
 }
 
 PageTransitions();
